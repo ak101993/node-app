aabc8b56d05753f7d5030046f3cf302c07bbee1d
akshay <akshay.akskumar10@gmail.com>
2017-06-11 01:10:31 +0530

    new commit

--- server/server.js
+++ server/server.js
@@ -18,6 +18,21 @@ io.on('connection',(socket)=>{
     //    from: 'a.kumar',
     //    text: 'This is return mail'
     // });
+
+    // create a emit event to show the greeting message of the admin
+    socket.emit('newMessage',{
+          from: 'Admin',
+          text: 'Welcome to chat box',
+          createdAt: new Date().getTime()
+    });
+    //create a broadcast event to show the the user who joined the chat
+
+    socket.broadcast.emit('newMessage',{
+      from: 'Admin',
+      text: 'New user joined',
+      createdAt: new Date().getTime()
+    });
+
     socket.on('createMessage',(createMessage)=>{
         console.log('createMessage',createMessage);
         io.emit('newMessage',{
@@ -25,6 +40,13 @@ io.on('connection',(socket)=>{
            text: createMessage.text,
            createdAt: new Date().getTime()
         });
+        //broadcast event helps to broadcast the message to  all the users
+        // except the one calling it
+        // socket.broadcast.emit('newMessage',{
+        //   from: createMessage.from,
+        //   text: createMessage.text,
+        //   createdAt: new Date().getTime()
+        // });
     })
     socket.on('disconnect',()=>{
         console.log('Disconnected from server');
