6077429c5ab76cbcc3257d06510a99c87dee20f4
akshay <akshay.akskumar10@gmail.com>
2017-06-10 23:10:57 +0530

    Emit newMessage

--- server/server.js
+++ server/server.js
@@ -13,6 +13,14 @@ app.use(express.static(publicPath));
 //port for deployment
 io.on('connection',(socket)=>{
     console.log(`New user connected ${socket}`);
+    //server emit a newMessage event and the client listen to that event
+    socket.emit('newMessage',{
+       from: 'a.kumar',
+       text: 'This is return mail'
+    });
+    socket.on('createMessage',(createMessage)=>{
+        console.log(createMessage)
+    })
     socket.on('disconnect',()=>{
         console.log('Disconnected from server');
     })
